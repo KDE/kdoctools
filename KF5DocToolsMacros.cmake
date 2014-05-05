eda49917a81cfb494e1c8e5af99ef32afa0c44ca
Aurélien Gâteau <agateau@kde.org>
2014-05-05 17:19:11 +0200

    Fix indentation

--- KF5DocToolsMacros.cmake
+++ KF5DocToolsMacros.cmake
@@ -74,133 +74,133 @@ macro(_SUGGEST_TARGET_NAME _out)
 endmacro()
 
 macro (KDOCTOOLS_CREATE_HANDBOOK _docbook)
-   get_filename_component(_input ${_docbook} ABSOLUTE)
-   set(_doc ${CMAKE_CURRENT_BINARY_DIR}/index.cache.bz2)
+    get_filename_component(_input ${_docbook} ABSOLUTE)
+    set(_doc ${CMAKE_CURRENT_BINARY_DIR}/index.cache.bz2)
 
-   #Bootstrap
-   if (_kdoctoolsBootStrapping)
-      set(_bootstrapOption "--srcdir=${KDocTools_BINARY_DIR}/src")
-   else ()
-      set(_bootstrapOption)
-   endif ()
-   set(_ssheet "${KDOCTOOLS_CUSTOMIZATION_DIR}/kde-chunk.xsl")
+    #Bootstrap
+    if (_kdoctoolsBootStrapping)
+        set(_bootstrapOption "--srcdir=${KDocTools_BINARY_DIR}/src")
+    else ()
+        set(_bootstrapOption)
+    endif ()
+    set(_ssheet "${KDOCTOOLS_CUSTOMIZATION_DIR}/kde-chunk.xsl")
 
-   file(GLOB _docs *.docbook)
+    file(GLOB _docs *.docbook)
 
-#   if (CMAKE_CROSSCOMPILING)
-#      set(IMPORT_MEINPROC5_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportMeinProc5Executable.cmake" CACHE FILEPATH "Point it to the export file of meinproc5 from a native build")
-#      include(${IMPORT_MEINPROC5_EXECUTABLE})
-#      set(KDOCTOOLS_MEINPROC_EXECUTABLE meinproc5)
-#   endif (CMAKE_CROSSCOMPILING)
+#    if (CMAKE_CROSSCOMPILING)
+#        set(IMPORT_MEINPROC5_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportMeinProc5Executable.cmake" CACHE FILEPATH "Point it to the export file of meinproc5 from a native build")
+#        include(${IMPORT_MEINPROC5_EXECUTABLE})
+#        set(KDOCTOOLS_MEINPROC_EXECUTABLE meinproc5)
+#    endif (CMAKE_CROSSCOMPILING)
 
-   add_custom_command(OUTPUT ${_doc}
-      COMMAND ${KDOCTOOLS_MEINPROC_EXECUTABLE} --check ${_bootstrapOption} --cache ${_doc} ${_input}
-      DEPENDS ${_docs} ${_ssheet}
-      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-   )
+    add_custom_command(OUTPUT ${_doc}
+        COMMAND ${KDOCTOOLS_MEINPROC_EXECUTABLE} --check ${_bootstrapOption} --cache ${_doc} ${_input}
+        DEPENDS ${_docs} ${_ssheet}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    )
 
-   _suggest_target_name(_targ)
-   set(_targ "${_targ}-handbook")
-   add_custom_target(${_targ} ALL DEPENDS ${_doc})
+    _suggest_target_name(_targ)
+    set(_targ "${_targ}-handbook")
+    add_custom_target(${_targ} ALL DEPENDS ${_doc})
 
-   if(KDOCTOOLS_ENABLE_HTMLHANDBOOK)
-      set(_htmlDoc ${CMAKE_CURRENT_SOURCE_DIR}/index.html)
-      add_custom_command(OUTPUT ${_htmlDoc}
-         COMMAND ${KDOCTOOLS_MEINPROC_EXECUTABLE} --check ${_bootstrapOption} -o ${_htmlDoc} ${_input}
-         DEPENDS ${_input} ${_ssheet}
-         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-      )
-      add_custom_target(htmlhandbook DEPENDS ${_htmlDoc})
-   endif(KDOCTOOLS_ENABLE_HTMLHANDBOOK)
+    if(KDOCTOOLS_ENABLE_HTMLHANDBOOK)
+        set(_htmlDoc ${CMAKE_CURRENT_SOURCE_DIR}/index.html)
+        add_custom_command(OUTPUT ${_htmlDoc}
+            COMMAND ${KDOCTOOLS_MEINPROC_EXECUTABLE} --check ${_bootstrapOption} -o ${_htmlDoc} ${_input}
+            DEPENDS ${_input} ${_ssheet}
+            WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+        )
+        add_custom_target(htmlhandbook DEPENDS ${_htmlDoc})
+    endif(KDOCTOOLS_ENABLE_HTMLHANDBOOK)
 
-   set(_args ${ARGN})
+    set(_args ${ARGN})
 
-   set(_installDest)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-         list(GET _args 1 _installDest )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-   endif(_args)
+    set(_installDest)
+    if(_args)
+        list(GET _args 0 _tmp)
+        if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+            list(GET _args 1 _installDest )
+            list(REMOVE_AT _args 0 1)
+        endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+    endif(_args)
 
-   get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "SUBDIR")
-         list(GET _args 1 dirname )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "SUBDIR")
-   endif(_args)
+    get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
+    if(_args)
+        list(GET _args 0 _tmp)
+        if("${_tmp}" STREQUAL "SUBDIR")
+            list(GET _args 1 dirname )
+            list(REMOVE_AT _args 0 1)
+        endif("${_tmp}" STREQUAL "SUBDIR")
+    endif(_args)
 
-   if(_installDest)
-      file(GLOB _images *.png)
-      install(FILES ${_doc} ${_docs} ${_images} DESTINATION ${_installDest}/${dirname})
-      # TODO symlinks on non-unix platforms
-      if (UNIX)
-         # execute some cmake code on make install which creates the symlink
-         install(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink \"${_installDest}/common\"  \"\$ENV{DESTDIR}${_installDest}/${dirname}/common\" )" )
-      endif (UNIX)
-   endif(_installDest)
+    if(_installDest)
+        file(GLOB _images *.png)
+        install(FILES ${_doc} ${_docs} ${_images} DESTINATION ${_installDest}/${dirname})
+        # TODO symlinks on non-unix platforms
+        if (UNIX)
+            # execute some cmake code on make install which creates the symlink
+            install(CODE "execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink \"${_installDest}/common\"  \"\$ENV{DESTDIR}${_installDest}/${dirname}/common\" )" )
+        endif (UNIX)
+    endif(_installDest)
 
 endmacro (KDOCTOOLS_CREATE_HANDBOOK)
 
 
 macro (KDOCTOOLS_CREATE_MANPAGE _docbook _section)
-   get_filename_component(_input ${_docbook} ABSOLUTE)
-   get_filename_component(_base ${_input} NAME)
+    get_filename_component(_input ${_docbook} ABSOLUTE)
+    get_filename_component(_base ${_input} NAME)
 
-   string(REGEX REPLACE "\\.${_section}\\.docbook$" "" _base ${_base})
+    string(REGEX REPLACE "\\.${_section}\\.docbook$" "" _base ${_base})
 
-   set(_doc ${CMAKE_CURRENT_BINARY_DIR}/${_base}.${_section})
-   # sometimes we have "man-" prepended
-   string(REGEX REPLACE "/man-" "/" _outdoc ${_doc})
+    set(_doc ${CMAKE_CURRENT_BINARY_DIR}/${_base}.${_section})
+    # sometimes we have "man-" prepended
+    string(REGEX REPLACE "/man-" "/" _outdoc ${_doc})
 
-   #Bootstrap
-   if (_kdoctoolsBootStrapping)
-      set(_bootstrapOption "--srcdir=${KDocTools_BINARY_DIR}/src")
-   else ()
-      set(_bootstrapOption)
-   endif ()
-   set(_ssheet "${KDOCTOOLS_CUSTOMIZATION_DIR}/kde-include-man.xsl")
+    #Bootstrap
+    if (_kdoctoolsBootStrapping)
+        set(_bootstrapOption "--srcdir=${KDocTools_BINARY_DIR}/src")
+    else ()
+        set(_bootstrapOption)
+    endif ()
+    set(_ssheet "${KDOCTOOLS_CUSTOMIZATION_DIR}/kde-include-man.xsl")
 
-#   if (CMAKE_CROSSCOMPILING)
-#      set(IMPORT_MEINPROC5_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportMeinProc5Executable.cmake" CACHE FILEPATH "Point it to the export file of meinproc5 from a native build")
-#      include(${IMPORT_MEINPROC5_EXECUTABLE})
-#      set(KDOCTOOLS_MEINPROC_EXECUTABLE meinproc5)
-#   endif (CMAKE_CROSSCOMPILING)
+#    if (CMAKE_CROSSCOMPILING)
+#        set(IMPORT_MEINPROC5_EXECUTABLE "${KDE_HOST_TOOLS_PATH}/ImportMeinProc5Executable.cmake" CACHE FILEPATH "Point it to the export file of meinproc5 from a native build")
+#        include(${IMPORT_MEINPROC5_EXECUTABLE})
+#        set(KDOCTOOLS_MEINPROC_EXECUTABLE meinproc5)
+#    endif (CMAKE_CROSSCOMPILING)
 
-   add_custom_command(OUTPUT ${_outdoc}
-      COMMAND ${KDOCTOOLS_MEINPROC_EXECUTABLE} --stylesheet ${_ssheet} --check ${_bootstrapOption} ${_input}
-      DEPENDS ${_input} ${_ssheet}
-   )
-   _suggest_target_name(_targ)
-   set(_targ "${_targ}-manpage-${_base}")
-   add_custom_target(${_targ} ALL DEPENDS "${_outdoc}")
+    add_custom_command(OUTPUT ${_outdoc}
+        COMMAND ${KDOCTOOLS_MEINPROC_EXECUTABLE} --stylesheet ${_ssheet} --check ${_bootstrapOption} ${_input}
+        DEPENDS ${_input} ${_ssheet}
+    )
+    _suggest_target_name(_targ)
+    set(_targ "${_targ}-manpage-${_base}")
+    add_custom_target(${_targ} ALL DEPENDS "${_outdoc}")
 
-   set(_args ${ARGN})
+    set(_args ${ARGN})
 
-   set(_installDest)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-         list(GET _args 1 _installDest )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
-   endif(_args)
+    set(_installDest)
+    if(_args)
+        list(GET _args 0 _tmp)
+        if("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+            list(GET _args 1 _installDest )
+            list(REMOVE_AT _args 0 1)
+        endif("${_tmp}" STREQUAL "INSTALL_DESTINATION")
+    endif(_args)
 
-   get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
-   if(_args)
-      list(GET _args 0 _tmp)
-      if("${_tmp}" STREQUAL "SUBDIR")
-         list(GET _args 1 dirname )
-         list(REMOVE_AT _args 0 1)
-      endif("${_tmp}" STREQUAL "SUBDIR")
-   endif(_args)
+    get_filename_component(dirname ${CMAKE_CURRENT_SOURCE_DIR} NAME_WE)
+    if(_args)
+        list(GET _args 0 _tmp)
+        if("${_tmp}" STREQUAL "SUBDIR")
+            list(GET _args 1 dirname )
+            list(REMOVE_AT _args 0 1)
+        endif("${_tmp}" STREQUAL "SUBDIR")
+    endif(_args)
 
-   if(_installDest)
-      install(FILES ${_outdoc} DESTINATION ${_installDest}/man${_section})
-   endif(_installDest)
+    if(_installDest)
+        install(FILES ${_outdoc} DESTINATION ${_installDest}/man${_section})
+    endif(_installDest)
 endmacro (KDOCTOOLS_CREATE_MANPAGE)
 
 
