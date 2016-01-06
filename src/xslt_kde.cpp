cbd56540d0fd3a9a4b46b621c10a0b619e57ed46
Luigi Toscano <luigi.toscano@tiscali.it>
2016-01-06 22:28:52 +0100

    Allow to disable KArchive for internal usage
    Most of the code was already prepared to compile without the KArchive
    dependency (including the MEINPROC_NO_KARCHIVE flag).
    This mode should be only enabled on KDE infrastructure, as the compressed
    cache (the only added feature coming from KArchive) is not used by the
    generator of the documentation website.
    A warning is printed when the mode is enabled.
    The saveToCache function is preserved, even if without code, to minimize
    the changes required.
    
    CHANGELOG: Allow minimal mode (no KArchive), only for KDE infrastructure
    REVIEW: 126648

--- src/xslt_kde.cpp
+++ src/xslt_kde.cpp
@@ -1,8 +1,13 @@
 #include "xslt_kde.h"
+#ifndef MEINPROC_NO_KARCHIVE
 #include <kfilterdev.h>
+#else
+#include <QDebug>
+#endif
 
 bool saveToCache(const QString &contents, const QString &filename)
 {
+#ifndef MEINPROC_NO_KARCHIVE
     KFilterDev fd(filename);
 
     if (!fd.open(QIODevice::WriteOnly)) {
@@ -11,5 +16,11 @@ bool saveToCache(const QString &contents, const QString &filename)
 
     fd.write(contents.toUtf8());
     fd.close();
+#else
+    qWarning() << "This function is dummy because KArchive support has been disabled. "
+               "This mode should be enabled only for specific usage on KDE infrastructure.";
+    Q_UNUSED(contents)
+    Q_UNUSED(filename)
+#endif
     return true;
 }
