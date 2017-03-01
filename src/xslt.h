8439c0a9761f4fec8913c47b90a10b4e5e51e6c5
Luigi Toscano <luigi.toscano@tiscali.it>
2017-03-01 22:33:59 +0100

    Remove the KF5::XsltKde static library
    Superseded by the dynamic library. This is an exception in the usual
    Frameworks support policy, but the static library was an exception
    too.

--- src/xslt.h
+++
@@ -1,9 +0,0 @@
-#ifndef _MEIN_XSLT_H_
-#define _MEIN_XSLT_H_
-
-#include "docbookxslt.h"
-#include "docbookxslt_p.h"
-
-using namespace KDocTools;
-
-#endif
--- src/xslt_kde.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MEIN_XSLT_KDE_H_
-#define _MEIN_XSLT_KDE_H_
-
-#include <QtCore/QString>
-
-bool saveToCache(const QString &contents, const QString &filename);
-
-#endif
