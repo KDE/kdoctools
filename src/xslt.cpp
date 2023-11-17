0a90be000709f9dd85936ffc6a5415684f650c3a
Haochen Tong <i@hexchain.org>
2023-11-17 14:54:23 +0800

    Fix build with libxml2 v2.12

--- src/xslt.cpp
+++ src/xslt.cpp
@@ -14,6 +14,7 @@
 #include <libxml/parser.h>
 #include <libxml/parserInternals.h>
 #include <libxml/xmlIO.h>
+#include <libxml/xmlsave.h>
 #include <libxslt/transform.h>
 #include <libxslt/xsltInternals.h>
 #include <libxslt/xsltconfig.h>
